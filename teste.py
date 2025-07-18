2512d76d4422a374efa92f62c2f152fa10a44b39
Leonardo <barbosa.olivera09@gmail.com>
2025-07-18 19:41:53 -0300

    Segundo commit

---
+++ teste.py
@@ -0,0 +1,7 @@
+# Programa simples de saudação
+
+# Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+
+# Exibe uma mensagem de boas-vindas
+print(f"Olá, {nome}! Seja bem-vindo ao Python.")
